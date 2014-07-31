2566c7b86a139d7c400c7a745c0b488310d3140c
kauldev <kaul_dev@outlook.com>
2014-07-31 19:14:31 -0400

    implemented pagination

--- tutorial_blog_ng/tutorial_blog_ng/urls.py
+++ tutorial_blog_ng/tutorial_blog_ng/urls.py
@@ -11,5 +11,5 @@ urlpatterns = patterns('',
     url(r'^admin/', include(admin.site.urls)),
 
     #Blog urls
-    url(r'^.*$', include('blogengine.urls')),
+    url(r'', include('blogengine.urls')),
 )
